bc95b2779684748f309951202136d4bfe5520241
Vasily Zavitaev <zavitayte85@gmail.com>
2019-05-30 14:57:19 +0300

    Удалили ненужный код

--- ld.py
+++ ld.py
@@ -1,3 +1,14 @@
-phones = ["iphone" , "samsung" , "xiaomi"]
+def discounted(price,discount):
+    price = abs(float(price))
+    discount = abs (float(discount))
 
-print(phones[-1])
+    price = 1000
+    discount = 15
+
+    if discount >= 100:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price*discount/100)
+
+print (price_with_discount)
+    
